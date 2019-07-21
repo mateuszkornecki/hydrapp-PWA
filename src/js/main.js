486e2c5808d13546611603a0191cf9c4bf6634a2
Mateusz Kornecki <mateusz.kornecki@gmail.com>
2019-07-21 18:09:56 +0200

    feat: added history

--- src/js/main.js
+++ src/js/main.js
@@ -16,30 +16,54 @@ if ('serviceWorker' in navigator) {
 
 // place your code below
 
-
 const counterNumber = document.querySelector(".counter__number--js");
 const buttonAdd = document.querySelector(".button__add--js");
 const buttonRemove = document.querySelector(".button__remove--js");
 const key = new Date().toISOString().slice(0, 10)
-let value = 0;
-counterNumber.innerHTML = value;
+const history = document.querySelector(".history");
+
+
+let counterValue = 0;
+counterNumber.innerHTML = counterValue;
 
 buttonAdd.addEventListener('click', (e) => {
-    if (value < 99) {
+    if (counterValue < 99) {
         counterNumber.innerHTML++;
-        value++;
-        localStorage.setItem(key, value);
+        counterValue++;
+        localStorage.setItem(key, counterValue);
     } else {
         console.log('Nie możliwe!')
     }
 })
 
 buttonRemove.addEventListener('click', (e) => {
-    if (value > 0) {
+    if (counterValue > 0) {
         counterNumber.innerHTML--;
-        value--;
-        localStorage.setItem(key, value);
+        counterValue--;
+        localStorage.setItem(key, counterValue);
     } else {
         console.log('Nie wypiłeś nawet jednej szklanki!');
     }
 })
+
+//! TESTY TRWAJĄ
+//? ---------
+// localStorage.setItem('test', 'wartość');
+// localStorage.setItem('test1', 'wartość2');
+// localStorage.setItem('test3', 'wartość3');
+const lama = Object.entries(localStorage);
+console.log(lama);
+console.log(lama.length);
+
+// lama.forEach((element) => {
+//     history.appendChild(li);
+//     li.innerHTML = element;
+//     console.log(element);
+// });
+
+for (let i = 0; i < lama.length; i++) {
+    const li = document.createElement('li');
+    li.id = i;
+    history.appendChild(li);
+    li.textContent = lama[i];
+}
